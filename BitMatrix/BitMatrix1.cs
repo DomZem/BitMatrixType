7657c7e6ef42472226006e98abb104e7d2f984ca
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-04-17 15:24:56 +0200

    Override ToString() for BitMatrix class

---
+++ BitMatrix/BitMatrix1.cs
@@ -0,0 +1,21 @@
+﻿namespace BitMatrix
+{
+    partial class BitMatrix
+    {
+        public override string ToString()
+        {
+            string result = "";
+                
+            for(int i = 0; i < NumberOfRows; i++)
+            {
+                for(int j = 0; j < NumberOfColumns; j++)
+                {
+                    result += (data[i] == false ? "0" : "1");
+                }
+                result += $"{Environment.NewLine}";
+            }
+            
+            return result;
+        }
+    }
+}
--- BitMatrix/Program.cs
+++ BitMatrix/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            // macierz bitów 4x3 wypełniona
+            // domyślną wartością
+            var m = new BitMatrix(4, 3);
+            Console.WriteLine(m.ToString());
+
+            // macierz bitów 3x4 wypełniona 1
+            m = new BitMatrix(3, 4, 1);
+            Console.WriteLine(m);
         }
     }
 }
